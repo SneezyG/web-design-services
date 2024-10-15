410879dc511ce5447a0d040aa7cce565b2ead943
sneezyg <ahmadgbolly97@gmail.com>
2024-10-15 18:55:57 +0100

    update

--- asset/menu.js
+++ asset/menu.js
@@ -1,20 +1,28 @@
 
 let menuBtn = document.querySelector(".menu-btn");
 let menuLinks = document.querySelector(".links");
+let showMenuBtn = document.querySelector(".show-menu");
+let hideMenuBtn = document.querySelector(".hide-menu");
 
 let menuLinkVisibility = false;
 
 menuBtn.addEventListener('click', () => {
   if (menuLinkVisibility) {
     menuLinks.style.display = "none";
+    showMenuBtn.style.display = "block";
+    hideMenuBtn.style.display = "none";
   }
   else {
     menuLinks.style.display = "block";
+    showMenuBtn.style.display = "none";
+    hideMenuBtn.style.display = "block";
   }
   menuLinkVisibility = !menuLinkVisibility;
 })
 
 document.addEventListener("scroll", () => {
   menuLinks.style.display = "none";
+  hideMenuBtn.style.display = "none";
+  showMenuBtn.style.display = "block";
   menuLinkVisibility = false;
 })
